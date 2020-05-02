751b087ee6288e5467b409a401b2b80279bc59da
Jaroslav Gor <gjarik@gmail.com>
2020-05-02 22:03:13 +0300

    UAParser extension to read parsers from YAML or JSON dynamically

--- uaparser/os.go
+++ uaparser/os.go
@@ -5,7 +5,7 @@ type Os struct {
 	Major      string
 	Minor      string
 	Patch      string
-	PatchMinor string `yaml:"patch_minor"`
+	PatchMinor string `yaml:"patch_minor" json:"patch_minor" bson:"patch_minor"`
 }
 
 func (parser *osParser) Match(line string, os *Os) {
